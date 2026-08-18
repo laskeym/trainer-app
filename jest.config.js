85c14e37d67a9e8d06f8313b495696e53c7c9a15
Mark Laskey <laskeymark0413@gmail.com>
2026-08-18 03:28:31 -0400

    Configured the project to separate the test suite use of regular Jest and jest-expo. Updated associated tests and configs.

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+// jest.config.js
+module.exports = {
+  projects: [
+    {
+      displayName: 'components',
+      preset: 'jest-expo',
+      transformIgnorePatterns: [
+        "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
+      ],
+      testMatch: ['<rootDir>/__tests__/components/**/*.test.js'],
+    },
+    {
+      displayName: 'integration',
+      testEnvironment: 'node',
+      transform: {
+        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+      },
+      testMatch: [
+        '<rootDir>/__tests__/unit/**/*.test.js',
+        '<rootDir>/__tests__/integration/**/*.test.js',
+      ],
+    },
+  ],
+}
