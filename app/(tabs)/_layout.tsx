7418e32ad3d239bc022e6d1537cc973882ce6402
Mark Laskey <laskeymark0413@gmail.com>
2026-08-17 22:09:31 -0400

    Initial project setup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -20,7 +20,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
+          title: 'Trainer App',
           tabBarIcon: ({ color }) => (
             <SymbolView
               name={{
