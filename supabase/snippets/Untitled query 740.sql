70385867c68424e279e6086a669ef21d7a68413e
Mark Laskey <laskeymark0413@gmail.com>
2026-08-19 03:04:02 -0400

    Stop tracking and ignore supabase/snippets/

--- supabase/snippets/Untitled query 740.sql
+++
@@ -1,1 +0,0 @@
-grant select, insert, update, delete on client to authenticated;
--- supabase/snippets/Untitled query 954.sql
+++
@@ -1,5 +0,0 @@
-select grantee, privilege_type
-from information_schema.role_table_grants
-where table_name = 'client'
-and grantee = 'authenticated'
-order by privilege_type;
