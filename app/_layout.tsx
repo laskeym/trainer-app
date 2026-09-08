87ef14aa2787df3179babdae4e598e5cd04a160d
Mark Laskey <laskeymark0413@gmail.com>
2026-09-08 10:53:21 -0400

    Implementing logging of exercise sets for a given client workout session

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -96,6 +96,9 @@ function RootLayoutNav() {
           <Stack.Screen name="session/[id]" options={{ headerShown: false }} />
         </Stack.Protected>
         <Stack.Protected guard={!!session}>
+          <Stack.Screen name="session/[id]/exercise/[sessionExerciseId]" options={{ headerShown: false }} />
+        </Stack.Protected>
+        <Stack.Protected guard={!!session}>
           <Stack.Screen
             name="templates/new"
             options={{
